c782d4043f89ee1f35044d6fbfbf961f1f4c1fc8
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-10 11:06:20 -0300

    Test code refactoring

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,38 +1,41 @@
 class LoginRouter {
-	route(httpRequest) {
-		if(!httpRequest.body.email || !httpRequest.body.password)
-			return { 
-				statusCode: 400, 
-			};
-	}
+    route(httpRequest) {
+        const { email, password } = httpRequest.body;
+
+        if(!email || !password) {
+            return { 
+                statusCode: 400,
+            };
+        }
+    }
 }
 
 describe("Login Router", () =>  {
-	test("Should return status 400 if no email is provided", () =>  {
-		const sut = new LoginRouter();
-		const httpRequest = {
-			body: {
-				password: "any_password",
-			},
-		};
+    test("Should return status 400 if no email is provided", () =>  {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                password: "any_password",
+            },
+        };
 
-		const httpResponse = sut.route(httpRequest);
+        const httpResponse = sut.route(httpRequest);
 
-		expect(httpResponse.statusCode).toBe(400);
-	});
+        expect(httpResponse.statusCode).toBe(400);
+    });
 });
 
 describe("Loguin Router", () => {
-	test("Should return status 400 if no password is provided", () => {
-		const sut = new LoginRouter();
-		const httpRequest = {
-			body: {
-				email: "any_email@email.com",
-			},
-		};
+    test("Should return status 400 if no password is provided", () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                email: "any_email@email.com",
+            },
+        };
 
-		const httpResponse = sut.route(httpRequest);
+        const httpResponse = sut.route(httpRequest);
 
-		expect(httpResponse.statusCode).toBe(400);
-	});
+        expect(httpResponse.statusCode).toBe(400);
+    });
 });
