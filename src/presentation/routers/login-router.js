28741b96d08ff93c5fccbf8ac6bdc5e9a61a944f
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-19 12:17:37 -0300

    TEST - Ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -22,7 +22,7 @@ class LoginRouter {
         if(!accessToken) 
             return HttpResponse.unauthorizedError();
 
-        return HttpResponse.ok();
+        return HttpResponse.ok({ accessToken });
     }
 }
 
