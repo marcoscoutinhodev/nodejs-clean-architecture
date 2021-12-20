200795503f63546b658bc846fee5e78570b74377
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-20 13:23:41 -0300

    TEST - Ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,12 +1,18 @@
 const { MissingParamError } = require("../../utils/errors");
 
 class AuthUseCaseSpy {
+    constructor (loadUserByEmailRepository) {
+        this.loadUserByEmailRepository = loadUserByEmailRepository;
+    }
+
     async auth (email, password) {
         if(!email)
             throw new MissingParamError("email");
 
         if(!password)
             throw new MissingParamError("password");
+
+        await this.loadUserByEmailRepository.load(email);
     }
 }
 
@@ -27,5 +33,19 @@ describe("Auth UseCase", () => {
         const promite = sut.auth("valid_email@email.com", undefined);
 
         expect(promite).rejects.toThrow(new MissingParamError("password"));
+    });
+
+    test("Should call LoadUserByEmailRepository with correct email", async () => {
+        class LoadUserByEmailRepositorySpy {
+            async load (email) {
+                this.email = email;
+            }
+        }
+        
+        const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+        const sut = new AuthUseCaseSpy(loadUserByEmailRepositorySpy);
+        await sut.auth("any_email@email.com", "any_password");
+
+        expect(loadUserByEmailRepositorySpy.email).toBe("any_email@email.com");
     });
 });
