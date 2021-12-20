2f65b0f80792d2803c4c734d937e93111ed954eb
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-20 19:21:13 -0300

    TEST - Ensure AuthUseCase throws if no dependency is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,5 +1,5 @@
 const AuthUseCase = require("./auth-usecase");
-const { MissingParamError, InvalidParamError } = require("../../utils/errors");
+const { MissingParamError } = require("../../utils/errors");
 
 const makeEncrypter = () => {
     class EncrypterSpy {
@@ -91,18 +91,25 @@ describe("Auth UseCase", () => {
         expect(loadUserByEmailRepositorySpy.email).toBe("any_email@email.com");
     });
 
+    test("Should throw if no dependency is provided", async () => {
+        const sut = new AuthUseCase();
+        const promise = sut.auth("any_email@email.com", "any_password");
+
+        expect(promise).rejects.toThrow();
+    });
+
     test("Should throw if no LoadUserByEmailRepository is provided", async () => {
         const sut = new AuthUseCase({});
         const promise = sut.auth("any_email@email.com", "any_password");
 
-        expect(promise).rejects.toThrow(new MissingParamError("loadUserByEmailRepository"));
+        expect(promise).rejects.toThrow();
     });
 
     test("Should throw if LoadUserByEmail has no load method", async () => {
         const sut = new AuthUseCase({ loadUserByEmailRepository: {} });
         const promise = sut.auth("any_email@email.com", "any_password");
 
-        expect(promise).rejects.toThrow(new InvalidParamError("loadUserByEmailRepository"));
+        expect(promise).rejects.toThrow();
     });
 
     test("Should return null if an invalid email is provided", async () => {
