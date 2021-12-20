4d2e855df8e1beb9745209446e64f20f6e5e43c0
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-20 13:29:38 -0300

    Refact sut factory method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -17,33 +17,38 @@ class AuthUseCaseSpy {
 }
 
 const makeSut = () => {
-    return new AuthUseCaseSpy();
+    class LoadUserByEmailRepositorySpy {
+        async load (email) {
+            this.email = email;
+        }
+    }
+    
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+    const sut = new AuthUseCaseSpy(loadUserByEmailRepositorySpy);
+
+    return {
+        sut,
+        loadUserByEmailRepositorySpy,
+    };
 };
 
 describe("Auth UseCase", () => {
     test("Should throw if no email is provided", async () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
         const promise = sut.auth(undefined, "any_password");
 
         expect(promise).rejects.toThrow(new MissingParamError("email"));
     });
 
     test("Should throw if no password is provided", async () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
         const promite = sut.auth("valid_email@email.com", undefined);
 
         expect(promite).rejects.toThrow(new MissingParamError("password"));
     });
 
     test("Should call LoadUserByEmailRepository with correct email", async () => {
-        class LoadUserByEmailRepositorySpy {
-            async load (email) {
-                this.email = email;
-            }
-        }
-        
-        const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
-        const sut = new AuthUseCaseSpy(loadUserByEmailRepositorySpy);
+        const { sut, loadUserByEmailRepositorySpy } = makeSut();
         await sut.auth("any_email@email.com", "any_password");
 
         expect(loadUserByEmailRepositorySpy.email).toBe("any_email@email.com");
