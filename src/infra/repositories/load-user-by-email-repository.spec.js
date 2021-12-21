85de2a9cc7f6803e0811a61a7ae03ff674489153
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-21 17:32:24 -0300

    TEST - Ensure LoadUserByEmailRepository throws if email is not provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,5 +1,6 @@
 const MongoHelper = require("../helpers/mongo-helper");
 const LoadUserByEmailRepository = require("./load-user-by-email-repository");
+const { MissingParamError } = require("../../utils/errors/index");
 
 const makeSut = (db) => {
     const userModel = db.collection("users");
@@ -56,5 +57,12 @@ describe("LoadUserByEmail Repository", () => {
         const promise = sut.load("any_email@email.com");
 
         expect(promise).rejects.toThrow();
+    });
+
+    test("Should throws if email is not provided", async () => {
+        const { sut } = makeSut(db);
+        const promise = sut.load();
+
+        expect(promise).rejects.toThrow(new MissingParamError("email"));
     });
 });
