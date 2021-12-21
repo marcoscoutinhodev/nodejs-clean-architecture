4a54156e9464871254d2f664e7ded546b5b12952
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-21 15:01:06 -0300

    Move TokenGenerator to its own file

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,21 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { MissingParamError } = require("../errors/index");
+const TokenGenerator = require("./token-generator");
 
-class TokenGenerator {
-    constructor (secret) {
-        this.secret = secret;
-    }
-
-    async generate (id) {
-        if(!this.secret)
-            throw new MissingParamError("secret");
-
-        if(!id)
-            throw new MissingParamError("id");
-
-        return jwt.sign(id, this.secret);
-    }
-}
 
 const makeSut = () => {
     return new TokenGenerator("secret_key_development");
