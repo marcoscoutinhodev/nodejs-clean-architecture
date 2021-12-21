a1a95e71c9d8148580575afe7c0bf6600f977a71
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-21 14:40:55 -0300

    TEST - Ensure TokenGenerator returns a token if JWT returns token

---
+++ __mocks__/jsonwebtoken.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "any_token",
+    sign (id, secret) {
+        this.id = id;
+        this.secret = secret;
+        
+        return this.token;
+    },
+};
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,14 +1,25 @@
+require("dotenv/config");
+const jwt = require("jsonwebtoken");
+
 class TokenGenerator {
-    async generate () {
-        return null;
+    async generate (id) {
+        return jwt.sign(id, process.env.JWT_KEY);
     }
 }
 
 describe("Token Generator", () => {
     test("Should return null if JWT returns null", async () => {
         const sut = new TokenGenerator();
+        jwt.token = null;
         const token = await sut.generate("any_id");
 
         expect(token).toBeNull();
+    });
+
+    test("Should returns a token if JWT returns token", async () => {
+        const sut = new TokenGenerator();
+        const token = await sut.generate("any_id");
+
+        expect(token).toBe(jwt.token);
     });
 });
