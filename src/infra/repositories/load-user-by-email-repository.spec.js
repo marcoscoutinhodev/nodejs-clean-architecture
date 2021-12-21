3f9392346aa2dbcf170780480cec5e3b4772c12c
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-21 16:02:52 -0300

    Move sut creation to a factory helper method

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -12,6 +12,16 @@ class LoadUserByEmailRepository {
     }
 }
 
+const makeSut = (db) => {
+    const userModel = db.collection("users");
+    const sut = new LoadUserByEmailRepository(userModel);
+
+    return {
+        sut,
+        userModel,
+    };
+};
+
 describe("LoadUserByEmail Repository", () => {
     let client, db;
 
@@ -33,21 +43,18 @@ describe("LoadUserByEmail Repository", () => {
     });
 
     test("Should return null if user is not found", async () => {
-        const userModel = db.collection("users");
-
-        const sut = new LoadUserByEmailRepository(userModel);
+        const { sut } = makeSut(db);
         const user = await sut.load("invalid_email@email.com");
 
         expect(user).toBeNull();
     });
 
     test("Should return an user if user is found", async () => {        
-        const userModel = db.collection("users");
+        const { sut, userModel } = makeSut(db);
         await userModel.insertOne({
             email: "valid_email@email.com",
         });
 
-        const sut = new LoadUserByEmailRepository(userModel);
         const user = await sut.load("valid_email@email.com");
 
         expect(user.email).toBe("valid_email@email.com");
