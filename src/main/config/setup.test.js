387e4a60e0fcaf248a7a08b89f45d23b23c46391
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-22 15:26:11 -0300

    Enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,8 +7,20 @@ describe("App Setup", () => {
             res.send("");
         });
              
-        const response = await request(app).get("/");
+        const response = await request(app).get("/test_x_powered_by");
 
         expect(response.headers["x-powered-by"]).toBeUndefined();
+    });
+
+    test("Should enable CORS", async () => {
+        app.get("/test_cors", (req, res) => {
+            res.send("");
+        });
+
+        const response = await request(app).get("/tesr_cors");
+
+        expect(response.headers["access-control-allow-origin"]).toBe("*");
+        expect(response.headers["access-control-allow-methods"]).toBe("*");
+        expect(response.headers["access-control-allow-headers"]).toBe("*");
     });
 });
