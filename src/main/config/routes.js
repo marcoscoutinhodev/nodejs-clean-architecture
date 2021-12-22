891966906ecdfb0a81cfa9da3b912b1a86d8f9fc
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-22 20:28:15 -0300

    Improve variable name

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-const fb = require("fast-glob");
+const fg = require("fast-glob");
 
 const router = Router();
 
 module.exports = app => {
     app.use("/api", router);
-    fb.sync("**/src/main/routes/**.js").forEach(file => {
+    fg.sync("**/src/main/routes/**.js").forEach(file => {
         require(`../../../${file}`)(router);
     });
 };
