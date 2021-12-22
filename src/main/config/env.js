95fd8f960bb80dc6fa48288387b2547863716a3f
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2021-12-22 20:34:45 -0300

    Fix mongodb port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 
 module.exports = {
-    mongoUrl: process.env.MONGO_URL || "mongodb://localhost:xxx/clean-node-api",
+    mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
     serverPortDev: process.env.SERVER_PORT || 4001,
     jwtKeySecret: process.env.JWT_KEY_SECRET || "secret_dev",
 };
